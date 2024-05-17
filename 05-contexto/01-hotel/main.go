633388a0de53c81e2926db6d8c7177c82a5dc009
Lucas Volpato <lmaciel916@gmail.com>
2024-05-17 21:43:18 +0000

    entendendo conceitos basicos

--- 05-contexto/01-hotel/main.go
+++ 05-contexto/01-hotel/main.go
@@ -1,7 +1,29 @@
 package main
 
-func main(){
+import (
+	"context"
+	"fmt"
+	"time"
+)
 
-	//inicia ctx em branco
+func main() {
+
+	//inicia context em branco
 	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, time.Second*3)
+	defer cancel()
+	bookHotel(ctx)
+}
+
+func bookHotel(ctx context.Context) {
+
+	select {
+	// Done -> quando o case der match com o que foi estabelecido no context
+	case <-ctx.Done():
+		fmt.Println("Hotel booking cancelled. Timeout reached")
+		return
+	// caso passe antes do time do ctx
+	case <-time.After(1 * time.Second):
+		fmt.Println("Hotel booked.")
+	}
 }
