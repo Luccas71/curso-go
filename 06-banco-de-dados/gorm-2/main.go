f355ef6bea0b744a38e1796c07d955d1107edfe0
Lucas Volpato <lmaciel916@gmail.com>
2024-05-22 18:56:49 +0000

    has one

--- 06-banco-de-dados/gorm-2/main.go
+++ 06-banco-de-dados/gorm-2/main.go
@@ -8,11 +8,12 @@ import (
 )
 
 type Product struct {
-	ID         int `gorm:"primaryKey"`
-	Name       string
-	Price      float64
-	CategoryId int
-	Category   Category
+	ID           int `gorm:"primaryKey"`
+	Name         string
+	Price        float64
+	CategoryId   int
+	Category     Category
+	SerialNumber SerialNumber
 	//adc created_at, updated_at e deleted_at automaticos
 	gorm.Model
 }
@@ -22,29 +23,42 @@ type Category struct {
 	Name string
 }
 
+//has one => só pode pertencer a um
+type SerialNumber struct {
+	ID        int `gorm:"primaryKey"`
+	Number    string
+	ProductId int
+}
+
 func main() {
 	dns := "root:root@tcp(localhost:3306)/goexpert?charset=utf8mb4&parseTime=true&loc=Local"
 	db, err := gorm.Open(mysql.Open(dns), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
-	db.AutoMigrate(&Product{}, Category{})
+	db.AutoMigrate(&Product{}, Category{}, SerialNumber{})
 
 	//create category
 	// category := Category{Name: "Eletronicos"}
 	// db.Create(&category)
 
 	//create product
-	product := Product{
-		Name:       "Keyboard",
-		Price:      36.0,
-		CategoryId: 1,
-	}
-	db.Create(&product)
+	// product := Product{
+	// 	Name:       "Keyboard",
+	// 	Price:      36.0,
+	// 	CategoryId: 1,
+	// }
+	// db.Create(&product)
+
+	db.Create(&SerialNumber{
+		Number:    "123456",
+		ProductId: 1,
+	})
 
 	var products []Product
-	db.Preload("Category").Find(&products)
+	db.Preload("Category").Preload("SerialNumber").Find(&products)
 	for _, product := range products {
-		fmt.Println(product.Name, product.Category.Name)
+		fmt.Println(product.Name, product.Category.Name, product.SerialNumber.Number)
 	}
+
 }
