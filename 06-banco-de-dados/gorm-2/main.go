795bd7ddca119e9ae5f7c7266dee019ef68b30ca
Lucas Volpato <lmaciel916@gmail.com>
2024-05-23 00:15:21 +0000

    many2many

--- 06-banco-de-dados/gorm-2/main.go
+++ 06-banco-de-dados/gorm-2/main.go
@@ -8,27 +8,20 @@ import (
 )
 
 type Product struct {
-	ID           int `gorm:"primaryKey"`
-	Name         string
-	Price        float64
-	CategoryId   int
-	Category     Category
-	SerialNumber SerialNumber
+	ID    int `gorm:"primaryKey"`
+	Name  string
+	Price float64
+	//pode ter varias categorias
+	Categories []Category `gorm:"many2many:products_categories;"`
 	//adc created_at, updated_at e deleted_at automaticos
 	gorm.Model
-}
-
-type SerialNumber struct {
-	ID        int `gorm:"primaryKey"`
-	Number    string
-	ProductId int
 }
 
 type Category struct {
 	ID   int `gorm:"primaryKey"`
 	Name string
 	//pode ter varios produtos
-	Products []Product
+	Products []Product `gorm:"many2many:products_categories;"`
 }
 
 func main() {
@@ -37,35 +30,33 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	db.AutoMigrate(&Product{}, Category{}, SerialNumber{})
+	db.AutoMigrate(&Product{}, Category{})
 
 	//create category
-	// category := Category{Name: "Cozinha"}
-	// db.Create(&category)
+	category1 := Category{Name: "Cozinha"}
+	db.Create(&category1)
+
+	category2 := Category{Name: "Eletronicos"}
+	db.Create(&category2)
 
 	// //create product
-	// product := Product{
-	// 	Name:       "Panela",
-	// 	Price:      32.60,
-	// 	CategoryId: 1,
-	// }
-	// db.Create(&product)
-
-	// //create serialNumber
-	// db.Create(&SerialNumber{
-	// 	Number:    "123",
-	// 	ProductId: 1,
-	// })
+	product := Product{
+		Name:       "Panela",
+		Price:      32.60,
+		Categories: []Category{category1, category2},
+	}
+	db.Create(&product)
 
 	var categories []Category
-	err = db.Model(&Category{}).Preload("Products.SerialNumber").Find(&categories).Error
+	err = db.Model(&Category{}).Preload("Products").Find(&categories).Error
 	if err != nil {
 		panic(err)
 	}
+
 	for _, category := range categories {
 		fmt.Println(category.Name, ":")
 		for _, product := range category.Products {
-			fmt.Println("- ", product.Name, " - Serial Number: ", product.SerialNumber.Number)
+			fmt.Println(product.Name)
 		}
 	}
 }
