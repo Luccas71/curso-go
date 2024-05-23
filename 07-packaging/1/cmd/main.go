821a2db99ab480ac6c902881681e966d85fa673b
Lucas Volpato <lmaciel916@gmail.com>
2024-05-23 14:55:02 +0000

    mod init

---
+++ 07-packaging/1/cmd/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Luccas71/curso-go/1/math"
+)
+
+func main() {
+
+	m := math.Math{A: 1, B: 2}
+	fmt.Println(m.Add())
+}
---
+++ 07-packaging/1/go.mod
@@ -0,0 +1,3 @@
+module github.com/Luccas71/curso-go/1
+
+go 1.22.2
