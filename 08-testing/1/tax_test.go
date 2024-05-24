60eae8f8cab1691e3e03d7ed602c4286af8a3f63
Lucas Volpato <lmaciel916@gmail.com>
2024-05-24 17:53:01 +0000

    test batch

--- 08-testing/1/tax_test.go
+++ 08-testing/1/tax_test.go
@@ -12,3 +12,24 @@ func TestCalculatetax(t *testing.T) {
 		t.Errorf("Expected %f but got %f", expected, result)
 	}
 }
+
+// testando varios valores de um vez
+func TestCalcTexBatch(t *testing.T) {
+	type calcTex struct {
+		amount, expect float64
+	}
+
+	table := []calcTex{
+		{500.0, 5.0},
+		{1000.0, 10.0},
+		{1200.0, 10.0},
+		{120.0, 5.0},
+	}
+
+	for _, item := range table {
+		result := CalculateTax(item.amount)
+		if result != item.expect {
+			t.Errorf("Expected %f but got %f", item.expect, result)
+		}
+	}
+}
