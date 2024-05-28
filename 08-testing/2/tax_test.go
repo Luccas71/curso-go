d867039c20376b5a561d6c37820bf2a0ebbfe2fe
Lucas Volpato <lmaciel916@gmail.com>
2024-05-28 17:51:50 +0000

    testify 2

--- 08-testing/2/tax_test.go
+++ 08-testing/2/tax_test.go
@@ -9,4 +9,13 @@ import (
 func TestCalculateTax(t *testing.T) {
 	tax := CalculateTax(100.0)
 	assert.Equal(t, 5.0, tax)
+
+	tax = CalculateTax(15000.25)
+	assert.Equal(t, 10.0, tax)
+	
+	tax = CalculateTax(25000.25)
+	assert.Equal(t, 20.0, tax)
+
+	tax = CalculateTax(0.0)
+	assert.Equal(t, 0.0, tax)
 }
