1e9e9b10ad84310babc46c3066d0644010943f6d
Lucas Volpato <lmaciel916@gmail.com>
2024-05-31 19:35:44 -0300

    separar handlers

---
+++ 09-API/internal/infra/webserver/handlers/product_handlers.go
@@ -0,0 +1,40 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/Luccas71/curso-go/09-API/internal/dto"
+	"github.com/Luccas71/curso-go/09-API/internal/entity"
+	"github.com/Luccas71/curso-go/09-API/internal/infra/database"
+)
+
+type ProductHandler struct {
+	ProductDB database.ProductInterface
+}
+
+func NewProductHandler(db database.ProductInterface) *ProductHandler {
+	return &ProductHandler{
+		ProductDB: db,
+	}
+}
+
+func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var product dto.CreateProductInput
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	p, err := entity.NewProduct(product.Name, product.Price)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	err = h.ProductDB.Create(p)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
